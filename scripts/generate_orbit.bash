1b64f202e9fa22b0c403adb65284c1ebb2af3e02
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-10-09 10:18:10 +0200

    Extended generate orbit script.

--- scripts/generate_orbit.bash
+++ scripts/generate_orbit.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 root=$(git rev-parse --show-toplevel)
-scene=${1:?"No scene given."}
-raytracer=${2:?"Path to RayTracer repo not given."}
-fmt=${3:-"${root}/orbit/cam-%03d"}
+raytracer=${1:?"Path to RayTracer repo not given."}
+scene=${2:?"No scene given."}
+fmt=${3:-"${root}/data/$(basename ${scene} .pbrt)/cam-%03d"}
 steps=${4:-5}
 
 dir=$(dirname ${fmt})
@@ -12,6 +12,7 @@ mkdir -p ${dir}
 scene=$(realpath ${scene})
 fmt=$(realpath ${fmt})
 
-# { cd ${raytracer} && bash ./scripts/pbrt_scene_orbit.sh ${scene} ${fmt} ${steps}; }
+{ cd ${raytracer} && bash ./scripts/pbrt_scene_orbit.sh ${scene} ${fmt} ${steps}; }
 python3 ${root}/scripts/pbrt_cameras.py ${dir}/*.pbrt ${dir}/camloc.json
 python3 ${root}/scripts/camera_edges.py ${dir}/camloc.json ${dir}/camgraph.json
+bash ${root}/scripts/generate_gif.sh ${fmt}.ppm ${steps}
